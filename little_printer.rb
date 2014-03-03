220aace01bc11a3fa9c880c55518b45f2a95aa23
Angus Mark <ngsmrk@gmail.com>
2014-03-03 14:39:52 +0000

    Use HTTPParty instead of CURL

--- little_printer.rb
+++ little_printer.rb
@@ -1,9 +1,15 @@
+require 'httparty'
+
 class LittlePrinter
+
   def initialize(code)
     @code = code
   end
 
   def message(msg)
-    `curl -X POST -d "html=<html><head><meta charset='utf-8'></head><body><p>#{msg}</p></body></html>" http://remote.bergcloud.com/playground/direct_print/#{@code}`
+    url = "http://remote.bergcloud.com/playground/direct_print/#{@code}"
+    HTTParty.post(url, :body => {:html => "<html><head><meta charset='utf-8'></head><body><p>#{msg}</p></body></html>"})
+    puts "Sent message: #{msg}"
+    #`curl -X POST -d "html=<html><head><meta charset='utf-8'></head><body><p>#{msg}</p></body></html>" http://remote.bergcloud.com/playground/direct_print/#{@code}`
   end
 end
