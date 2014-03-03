c8db952939b6ba907e540b64b66a87ff3a2bb455
Dragos Miron <dragos.miron@simplybusiness.co.uk>
2014-03-03 16:45:02 +0000

    printing last tweets sent to smackmypitchup

--- little_printer.rb
+++ little_printer.rb
@@ -9,7 +9,5 @@ class LittlePrinter
   def message(msg)
     url = "http://remote.bergcloud.com/playground/direct_print/#{@code}"
     HTTParty.post(url, :body => {:html => "<html><head><meta charset='utf-8'></head><body><p>#{msg}</p></body></html>"})
-    puts "Sent message: #{msg}"
-    #`curl -X POST -d "html=<html><head><meta charset='utf-8'></head><body><p>#{msg}</p></body></html>" http://remote.bergcloud.com/playground/direct_print/#{@code}`
   end
 end
--- twitter_server.rb
+++ twitter_server.rb
@@ -1,15 +1,20 @@
 require_relative 'client'
 require_relative 'little_printer'
-little_printer = LittlePrinter.new(ENV["LITTLE_PRINTER_ID"])
-client = LittleTwitter.new.client
-last_msg = ""
+@little_printer = LittlePrinter.new(ENV["LITTLE_PRINTER_ID"])
+@client = LittleTwitter.new.client
+@since_id = nil
+
 loop do
-  sleep 10
-  client.search("", :result_type => "recent", :geocode => "51.520767,-0.08737,1km").take(1).each do |tweet|
-    if last_msg != tweet.text
-      last_msg = tweet.text
-      payload = "Last tweet in a 1km radius of the office<br/>From: #{tweet.user.username}<br/>Tweet: #{tweet.text}"
-      little_printer.message(payload)
-    end
+  options = {:result_type => "recent"}
+  options.merge!(:since_id => @since_id) if @since_id
+
+  tweets = @client.search("#SmackMyPitchUp", options).take(10)
+  @since_id = tweets.first.id if tweets.first && tweets.first.id
+  tweets.each do |tweet|
+    payload = "From: #{tweet.user.username}<br/>Tweet: #{tweet.text}<br/>"
+    payload << "<img src='#{tweet.media.first.media_uri}' class='dither' width='380'/>" if tweet.media?
+    @little_printer.message(payload)
   end
+
+  sleep 5
 end
