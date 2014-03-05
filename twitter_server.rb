0dc99d18737e2bef1b444f27acea4f6cdd4d83ce
Dragos Miron <dragos.miron@simplybusiness.co.uk>
2014-03-05 09:45:12 +0000

    restyle of template

--- twitter_server.rb
+++ twitter_server.rb
@@ -9,13 +9,13 @@ loop do
   options = {:result_type => "recent"}
   options.merge!(:since_id => @since_id) if @since_id
 
-  tweets = @client.search(@search_criteria, options).take(1)
+  tweets = @client.search(@search_criteria, options).take(5)
   @since_id = tweets.first.id if tweets.first && tweets.first.id
   tweets.each do |tweet|
     payload = "From: #{tweet.user.username}<br/>Tweet: #{tweet.text}<br/>"
     payload << "<img src='#{tweet.media.first.media_uri}' class='dither' width='380'/>" if tweet.media?
-    @little_printer.message(payload)
+    @little_printer.message(payload)`
   end
 
-  sleep 5
+  sleep 30
 end
